524840d55045d9913ff00f120734b7066f005399
Spraylan <jonasalbrecht@gmail.com>
2024-10-14 03:28:04 -0500

    Stage I mk II

--- main.py
+++ main.py
@@ -4,18 +4,14 @@ from constants import *
 
 def main():
     pygame.init
-    
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+       
 
-    while screen:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0, rect=None, special_flags=0)
+        screen.fill("black")
         pygame.display.flip()
     
 
