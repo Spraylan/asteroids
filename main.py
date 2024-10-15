ad9aa57e2898ae0f79cef7ecc75ad9f21e3932df
Spraylan <jonasalbrecht@gmail.com>
2024-10-15 01:34:36 -0500

    Stage II

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 
 def main():
@@ -7,6 +9,7 @@ def main():
     clock = pygame.time.Clock
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
        
 
     while True:
@@ -14,8 +17,12 @@ def main():
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
     dt = clock.tick(60) / 1000
+
+    
     
     
 
